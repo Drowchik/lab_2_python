92d4406f79226b0fe1a83a2adfb2a9465f9150a6
natal <natal@CLOWN>
2023-10-11 09:05:47 +0400

    2_task_complete

--- task_1.py
+++ task_1.py
@@ -1,7 +1,10 @@
 import os
 import csv
 
-def write_info(rating) -> None :
+def write_info(rating: int) -> None :
+    '''
+        the function enters the full path, relative path and review rating
+    '''
     p=os.path.abspath('dataset')
     name_review=os.listdir(os.path.join(p, str(rating)))
     p1=os.path.relpath('dataset')
@@ -16,15 +19,15 @@ def get_absolute_path():
      name_review=os.listdir(p+'/1')
      
 def create_csv_file():
-     with open('annotation.csv', 'w') as csv_file:
+    '''
+        the function creates a csv file and enters the name of the columns
+    '''
+    with open('annotation.csv', 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=",", lineterminator='\r')
         writer.writerow( ["absolute_path", "relative path", "rating"])  
      
-def main():
+def run_task1():
     create_csv_file()  
     for i in range(1, 6):
         write_info(i)
 
-
-if __name__=='__main__':
-    main()
